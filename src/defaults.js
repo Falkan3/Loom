99ae0bcc5f1fa9d77509b75f050515156edec5bd
Adam Kocić <adam.kocic@hotmail.com>
2022-01-29 03:01:25 +0100

    Reduce input event delay variable setting

--- src/defaults.js
+++ src/defaults.js
@@ -60,6 +60,6 @@ export default {
     variables: {
         // eslint-disable-next-line max-len
         emailValidationRegex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        inputEventDelay: 500,
+        inputEventDelay: 200,
     },
 };
