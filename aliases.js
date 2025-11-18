37340cead35982174f466c470f716f4e57cc04ca
Adam <adam.kocic@trafficwatchdog.pl>
2025-11-18 15:21:11 +0100

    Add default export in aliases.js file, remove System.config function call

--- aliases.js
+++ aliases.js
@@ -1,17 +1,16 @@
-// eslint-disable-next-line no-undef
-System.config({
-    paths: {
-        '@src': './src',
-        '@src/*': './src/*',
-        '@libs': './src/libs',
-        '@libs/*': './src/libs/*',
-        '@components': './src/components',
-        '@components/*': './src/components/*',
-        '@core': './src/core',
-        '@core/*': './src/core/*',
-        '@utils': './src/utils',
-        '@utils/*': './src/utils/*',
-        '@assets': './src/assets',
-        '@assets/*': './src/assets/*'
-    }
-});
+const aliases = {
+    '@src': './src',
+    '@src/*': './src/*',
+    '@libs': './src/libs',
+    '@libs/*': './src/libs/*',
+    '@components': './src/components',
+    '@components/*': './src/components/*',
+    '@core': './src/core',
+    '@core/*': './src/core/*',
+    '@utils': './src/utils',
+    '@utils/*': './src/utils/*',
+    '@assets': './src/assets',
+    '@assets/*': './src/assets/*'
+};
+
+export default aliases;
