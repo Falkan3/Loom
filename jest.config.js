e2618e70c559e3a480b48dce9242583a99bda731
Adam <adam.kocic@trafficwatchdog.pl>
2025-11-18 15:23:01 +0100

    Update jest configuration to work with all js file extensions

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,22 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
     verbose: true,
-    testEnvironment: 'jsdom'
+    testEnvironment: 'jsdom',
+
+    // Explicitly list accepted extensions
+    moduleFileExtensions: ['js', 'json', 'jsx', 'mjs', 'ts', 'tsx'],
+
+    // Update the transform regex to match .mjs files
+    transform: {
+        // This regex matches: .js, .jsx, .mjs, .ts, .tsx
+        '^.+\\.[jt]sx?|mjs$': [
+            'babel-jest',
+            {
+                // This forces babel-jest to look up babel.config.js
+                babelrc: true,
+            }
+        ],
+    },
 };
 
 export default config;
